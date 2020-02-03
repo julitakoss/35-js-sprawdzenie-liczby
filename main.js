ac0aebae536b45c1ea24e4be334b0b543f6b298d
julita_koss@tlen.pl <julitakoss@github.com>
2020-02-03 11:23:49 +0100

    commit projektu

---
+++ main.js
@@ -0,0 +1,11 @@
+let n = 130;
+console.log(xyz(n));
+function xyz(n)
+{
+    if(n >100 && n< 200){
+        console.log('warunek spełniony');
+}
+    else {     
+        console.log('liczba nie znajduje się w przedziale');
+}
+}
